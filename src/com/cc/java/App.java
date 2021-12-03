701ea6ed1bf71a5f7ac18472e7cc24b761e95733
DonCerebro1 <85444482+DonCerebro1@users.noreply.github.com>
2021-12-03 11:59:03 +0100

    added isFemale

--- src/com/cc/java/App.java
+++ src/com/cc/java/App.java
@@ -4,16 +4,16 @@ public class App {
     
     public static void main(String[] args) {
 
-        Cat cat1 = new Cat("Grizabella", "white", 29); // Klasse --> Objekt / Instanz
+        Cat cat1 = new Cat("Grizabella", "white", 29,true); // Klasse --> Objekt / Instanz
 
         output("Name: " + cat1.getStringAttributes("#name"));
         output("Fellfarbe: " + cat1.getStringAttributes("#color"));
-        
+
         output("Alter: " + cat1.getAge());
 
         output("------------------------");
 
-        Cat cat2 = new Cat("Alonzo", "black", 35); // Klasse --> Objekt / Instanz
+        Cat cat2 = new Cat("Alonzo", "black", 35,false); // Klasse --> Objekt / Instanz
 
         output("Name: " + cat2.getStringAttributes("#name"));
         output("Fellfarbe: " + cat2.getStringAttributes("#color"));
--- src/com/cc/java/Cat.java
+++ src/com/cc/java/Cat.java
@@ -5,36 +5,37 @@ public class Cat {
     private String name;
     private String furColor;
     private int age;
+    private boolean isFemale;
 
-    public Cat(String name, String furColor, int age) {
+
+    public Cat(String name, String furColor, int age, boolean isFemale) {
         this.name = name;
         this.furColor = furColor;
         this.age = age;
+        this.isFemale = isFemale;
     }
 
     public String getStringAttributes(String flag){
-
         switch (flag) {
             case "#name":
                 return name;
                 
             case "#color":
                 return furColor;
-               
-
-        
+    
             default:
-            return "#!?";
-               
-        }
-
-
-        
-        
+            return "#!?"; 
+        }    
     }
 
-    public int getAge() {
-        return age;
+    public String getAge() {
+
+        if (isFemale) {
+            return "This is an inappropriate question!";
+        } else {
+            return Integer.toString(age);
+        }
+        
     }
 
   
