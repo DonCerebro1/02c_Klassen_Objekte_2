c1498e72f812e66a61413bf46c92debddce88ab1
DonCerebro1 <85444482+DonCerebro1@users.noreply.github.com>
2021-12-03 12:47:34 +0100

    updated

--- src/com/cc/java/Cat.java
+++ src/com/cc/java/Cat.java
@@ -1,11 +1,11 @@
 package com.cc.java;
 
 public class Cat {
-    
     private String name;
     private String furColor;
     private int age;
     private boolean isFemale;
+    private int counter;
 
 
     public Cat(String name, String furColor, int age, boolean isFemale) {
@@ -28,14 +28,32 @@ public class Cat {
         }    
     }
 
-    public String getAge() {
+    public String getAge(){
+        return checkCompliance();
+    }
 
+    // private damit keiner ausserhal der Klasse zugreifen oder ändern kann
+    private String checkCompliance() {
         if (isFemale) {
-            return "This is an inappropriate question!";
+            return checkEscalationlevel();
         } else {
             return Integer.toString(age);
         }
-        
+    }
+
+    private String checkEscalationlevel(){
+        counter++;
+
+        switch (counter) {
+            case 1:
+                return "This is an inappropriate question!";
+            case 2:
+                return "I´ve told you once!";  
+            case 3:
+                return "Talk to the hand!"; 
+            default:
+                return "1#+?!!!";
+        }
     }
 
   
